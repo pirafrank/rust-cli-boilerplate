cdff21629814b269bcd76911e77d3f1abb35bd90
Francesco Pira <dev@fpira.com>
2026-01-06 23:54:19 +0100

    feat: richer --version output

---
+++ src/constants.rs
@@ -0,0 +1,5 @@
+// version constants
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
+pub const COMMIT: &str = env!("GIT_COMMIT_HASH");
+pub const BUILD_DATE: &str = env!("BUILD_DATE");
+pub const COMPILE_C_LIB: &str = env!("C_LIB");
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod constants;
+use crate::constants::*;
 use anyhow::Result;
 use clap::{Parser, Subcommand};
 use clap_verbosity_flag::{InfoLevel, Verbosity};
@@ -10,7 +12,13 @@ use log::{debug, info};
     version = env!("CARGO_PKG_VERSION"),
     author = env!("CARGO_PKG_AUTHORS"),
     about = env!("CARGO_PKG_DESCRIPTION"),
-    long_about = None
+    long_version = long_version(),
+    help_template = "\n\n{name} - {about}\n\n\
+        {usage-heading} {usage}\n\n\
+        {all-args}{after-help}",
+    after_help = format!("For more information, visit: {}\n\n\
+        If you encounter any issues, please report them at:\n{}/issues\n",
+        env!("CARGO_PKG_REPOSITORY"), env!("CARGO_PKG_REPOSITORY")),
 )]
 struct Cli {
     /// Subcommand to execute
@@ -37,6 +45,23 @@ enum Commands {
         /// Message to echo
         message: String,
     },
+}
+
+/// Returns a static string containing the version information.
+/// It uses Box::leak to convert a String into a &'static str.
+/// This is a workaround to avoid using a global static variable.
+fn long_version() -> &'static str {
+    #[cfg(static_linking)]
+    let linking_type = "statically linked";
+    #[cfg(dynamic_linking)]
+    let linking_type = "dynamically linked";
+    Box::leak(
+        format!(
+            "\nVersion   : {}\nCommit    : {}\nBuild Date: {}\nBuilt with: {} ({})",
+            VERSION, COMMIT, BUILD_DATE, COMPILE_C_LIB, linking_type
+        )
+        .into_boxed_str(),
+    )
 }
 
 fn run() -> Result<()> {
